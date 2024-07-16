6d1190dac2505d2dcc6354ee3567d50b4b2d73e3
Rayuth You <rayuth@web-essentials.co>
2024-07-16 20:48:20 +0700

    Upgrade API 34

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class _MyHomePageState extends State<MyHomePage> {
               padding: const EdgeInsets.all(8),
               child: Text(
                 '$index',
-                style: Theme.of(context).textTheme.headline5,
+                style: Theme.of(context).textTheme.headlineSmall,
               ),
               color: Colors.blue[100],
             ),
